5e60ac8f9a3af52e7b27f3ef4716135f31b8df75
kaito-jiro <haidoushankou68@gmail.com>
2021-09-02 22:48:04 +0900

    環境設定1

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,15 @@
 // After: _app.tsx
 import '../styles/globals.css'
 import {AppProps} from "next/app";
+import { AuthContext, AuthProvider } from './src/auth/AuthProvider';
+import { Children } from 'react';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+      <AuthProvider>
+        <Component {...pageProps} />
+      </AuthProvider>
+    )
 }
 
 export default MyApp
